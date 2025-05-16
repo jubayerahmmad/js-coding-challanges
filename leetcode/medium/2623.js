f1799ee01e8d60c24462769b7e3390c2d772070b
jubayerahmmad <zubayersami510@gmail.com>
2025-05-16 11:53:26 +0600

    leetcode: solve 2623(medium) – memoize

---
+++ leetcode/medium/2623.js
@@ -0,0 +1,45 @@
+/**
+ * LeetCode 2623 – Memoize
+ * Difficulty: Medium
+ *
+ * Problem:
+ * Implement a memoization function. Given a function `fn`, return a memoized version of it.
+ * The memoized function should return cached results when called with the same arguments.
+ *
+ * Example:
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
+
+function memoize(fn) {
+  let cache = {};
+  return function (...args) {
+    const key = args.toString();
+
+    // cache.hasOwnProperty(key) --> best choice
+    // key in cache --> best choice
+    // cache[key] -->  won't work if the fn return 0 as its a falsy value
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    } else {
+      cache[key] = fn(...args);
+    }
+
+    return cache[key];
+  };
+}
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
+console.log(memoizedFn(1, -1)); // 0
+console.log(memoizedFn(1, -1)); // 0
+console.log(callCount); // 1
